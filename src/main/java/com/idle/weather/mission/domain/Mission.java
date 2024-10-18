4377893863f53a65425833024750799937b42f72
supportlaver <ak4686@naver.com>
2024-10-18 15:50:44 +0900

    feat: mission Entity 생성

--- src/main/java/com/idle/weather/mission/domain/Mission.java
+++ src/main/java/com/idle/weather/mission/domain/Mission.java
@@ -1,4 +1,18 @@
 package com.idle.weather.mission.domain;
 
+import lombok.Builder;
+import lombok.Getter;
+
+import java.util.List;
+
+@Getter
+@Builder
 public class Mission {
+    private Long id;
+    private String name;
+    private String description;
+    private int point;
+    private List<MissionHistory> missionHistories;
+    private String uploadFileName;
+    private String storeFileName;
 }
--- src/main/java/com/idle/weather/mission/repository/MissionRepositoryImpl.java
+++ src/main/java/com/idle/weather/mission/repository/MissionRepositoryImpl.java
@@ -1,5 +1,8 @@
 package com.idle.weather.mission.repository;
 
+import com.idle.weather.exception.BaseException;
+import com.idle.weather.exception.ErrorCode;
+import com.idle.weather.mission.domain.Mission;
 import com.idle.weather.mission.service.port.MissionRepository;
 import com.idle.weather.missionhistory.repository.MissionHistoryJpaRepository;
 import lombok.RequiredArgsConstructor;
@@ -7,6 +10,12 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 @RequiredArgsConstructor
-public class MissionRepositoryImpl  implements MissionRepository {
+public class MissionRepositoryImpl implements MissionRepository{
     private final MissionJpaRepository missionJpaRepository;
+
+    @Override
+    public Mission findById(Long id) {
+        return missionJpaRepository.findById(id)
+                .orElseThrow(() -> new BaseException(ErrorCode.NOT_FOUND_MISSION)).toDomain();
+    }
 }
