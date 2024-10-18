9f83381a4e0e32fe94de64c505897e89a16bf6d5
supportlaver <ak4686@naver.com>
2024-10-18 15:42:45 +0900

    feat: entity 추가

--- src/main/java/com/idle/weather/board/repository/BoardEntity.java
+++ src/main/java/com/idle/weather/board/repository/BoardEntity.java
@@ -1,4 +1,11 @@
 package com.idle.weather.board.repository;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+
+@Entity
 public class BoardEntity {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "board_id")
+    private Long id;
 }
