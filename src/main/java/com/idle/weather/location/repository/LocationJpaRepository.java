46f1b1e1f467f2570dfda6eb6f7b3bc7504a7b00
sunny.park <seolseee@gmail.com>
2024-10-22 22:43:06 +0900

    [feat] - 위치 jpa 레포지토리 추가

--- src/main/java/com/idle/weather/location/repository/LocationJpaRepository.java
+++ src/main/java/com/idle/weather/location/repository/LocationJpaRepository.java
@@ -1,4 +1,9 @@
 package com.idle.weather.location.repository;
 
-public interface LocationJpaRepository {
+import com.idle.weather.location.domain.Location;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LocationJpaRepository extends JpaRepository<Location, Long> {
 }
