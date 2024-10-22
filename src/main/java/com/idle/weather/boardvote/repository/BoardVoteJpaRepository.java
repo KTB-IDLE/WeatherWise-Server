535d8c083b1595350e73b23139ebe7a4206ffdc5
sunny.park <seolseee@gmail.com>
2024-10-22 22:41:17 +0900

    [feat] - 게시글 투표 jpa 레포지토리 추가

--- src/main/java/com/idle/weather/boardvote/repository/BoardVoteJpaRepository.java
+++ src/main/java/com/idle/weather/boardvote/repository/BoardVoteJpaRepository.java
@@ -1,4 +1,28 @@
 package com.idle.weather.boardvote.repository;
 
-public interface BoardVoteJpaRepository {
+import com.idle.weather.board.repository.BoardEntity;
+import com.idle.weather.boardvote.domain.BoardVote;
+import com.idle.weather.boardvote.domain.VoteType;
+import com.idle.weather.user.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface BoardVoteJpaRepository extends JpaRepository<BoardVote, Long> {
+
+    @Query("""
+            SELECT v
+            FROM BoardVote v
+            WHERE v.user = :user AND v.board = :board
+            """)
+    Optional<BoardVote> findCurrentVoteTypeByUserAndBoard(@Param("user") User user, @Param("board") BoardEntity board);
+
+    @Query("""
+            DELETE
+            FROM BoardVote v
+            WHERE v.user = :user AND v.board = :board
+            """)
+    void removeVote(@Param("user") User user, @Param("board") BoardEntity board);
 }
