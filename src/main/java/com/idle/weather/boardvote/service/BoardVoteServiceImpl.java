42fb0c4bd4ede248c19d0644933ec1aa6825a10f
sunny.park <seolseee@gmail.com>
2024-10-24 16:16:43 +0900

    [fix] - BoardVote Service, Controller 삭제

--- src/main/java/com/idle/weather/boardvote/service/BoardVoteServiceImpl.java
+++
@@ -1,8 +0,0 @@
-package com.idle.weather.boardvote.service;
-
-import com.idle.weather.boardvote.api.port.BoardVoteService;
-import org.springframework.stereotype.Service;
-
-@Service
-public class BoardVoteServiceImpl implements BoardVoteService {
-}
