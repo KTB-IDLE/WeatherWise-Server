910a9bd07cdb9f9cc8ea6a0aadf025468862f42f
supportlaver <ak4686@naver.com>
2024-10-25 10:22:34 +0900

    fix: @UserId 사용시 NPE 오류 해결

--- src/main/java/com/idle/weather/global/config/WebConfig.java
+++ src/main/java/com/idle/weather/global/config/WebConfig.java
@@ -1,11 +1,21 @@
 package com.idle.weather.global.config;
 
+import com.idle.weather.interceptor.UserIdArgumentResolver;
+import com.idle.weather.interceptor.UserIdInterceptor;
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
+    private final UserIdInterceptor userIdInterceptor;
+    private final UserIdArgumentResolver userIdArgumentResolver;
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
@@ -13,5 +23,15 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
+    }
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(userIdInterceptor)
+                .addPathPatterns("/**") // 모든 경로에 대해 적용
+                .excludePathPatterns("/exclude-path"); // 제외할 경로가 있으면 추가
+    }
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userIdArgumentResolver);
     }
 }
