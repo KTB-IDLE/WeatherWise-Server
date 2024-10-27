1abc602a3fc487371abda17401ecf2a387f5715c
supportlaver <ak4686@naver.com>
2024-10-27 20:21:13 +0900

    feat: API Response Time 출력 형태 설정

--- src/main/java/com/idle/weather/global/config/ObjectMapperConfig.java
+++ src/main/java/com/idle/weather/global/config/ObjectMapperConfig.java
@@ -1,6 +1,7 @@
 package com.idle.weather.global.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,6 +13,8 @@ public class ObjectMapperConfig {
     public ObjectMapper objectMapper() {
         ObjectMapper om = new ObjectMapper();
         om.registerModule(new JavaTimeModule());
+        // 시간 정보를 ISO 8601 문자열 형식으로 출력하도록 설정
+        om.disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
         return om;
     }
 }
