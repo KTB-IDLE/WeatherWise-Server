e7a474fba651c68717363d21a0cf8728071ab47e
sunny.park <seolseee@gmail.com>
2024-10-29 13:09:24 +0900

    [fix] - BoardVote DTO 투표 ID, 사용자 ID 추가 및 from 메서드 추가

--- src/main/java/com/idle/weather/boardvote/api/response/BoardVoteResponse.java
+++ src/main/java/com/idle/weather/boardvote/api/response/BoardVoteResponse.java
@@ -1,9 +1,15 @@
 package com.idle.weather.boardvote.api.response;
 
+import com.idle.weather.board.api.response.BoardResponse;
+import com.idle.weather.board.repository.BoardEntity;
+import com.idle.weather.boardvote.domain.BoardVote;
 import com.idle.weather.boardvote.domain.VoteType;
 import io.swagger.v3.oas.annotations.media.Schema;
 
 public record BoardVoteResponse(
+        @Schema(description = "투표 ID", example = "1")
+        Long voteId,
+
         @Schema(description = "사용자 ID", example = "1")
         Long userId,
 
@@ -13,4 +19,12 @@ public record BoardVoteResponse(
         @Schema(description = "투표 타입", example = "UPVOTE")
         VoteType voteType
 ) {
+        public static BoardVoteResponse from(BoardVote boardVote) {
+                return new BoardVoteResponse(
+                        boardVote.getVoteId(),
+                        boardVote.getBoard().getBoardId(),
+                        boardVote.getUser().getId(),
+                        boardVote.getVoteType()
+                );
+        }
 }
