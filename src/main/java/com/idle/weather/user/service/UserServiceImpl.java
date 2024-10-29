ab8d97d23443a3a9da221a906e5ef1a4069f6746
sunny.park <seolseee@gmail.com>
2024-10-29 13:15:19 +0900

    [fix] - 유저 반환타입 수정 및 닉네임 비밀번호 변경, 회원 탈퇴, 회원 재활성화 로직 추가

--- src/main/java/com/idle/weather/user/service/UserServiceImpl.java
+++ src/main/java/com/idle/weather/user/service/UserServiceImpl.java
@@ -1,6 +1,7 @@
 package com.idle.weather.user.service;
 
 import com.idle.weather.user.api.port.UserService;
+import com.idle.weather.user.api.response.UserResponse;
 import com.idle.weather.user.domain.User;
 import com.idle.weather.user.dto.AuthSignUpDto;
 import com.idle.weather.user.repository.UserEntity;
@@ -23,27 +24,67 @@ public class UserServiceImpl implements UserService {
 
     @Override
     @Transactional
-    public UserEntity signUp(AuthSignUpDto authSignUpDto) {
+    public UserResponse signUp(AuthSignUpDto authSignUpDto) {
         String encodedPass = passwordEncoder.encode(authSignUpDto.password());
         UserEntity newUser = UserEntity.signUp(authSignUpDto, encodedPass);
-
-        return userJpaRepository.save(newUser);
+        return UserResponse.from(userJpaRepository.save(newUser));
     }
 
     @Override
-    public Optional<UserEntity> findById(Long id) {
-        return userJpaRepository.findById(id);
+    public UserResponse findById(Long userId) {
+        UserEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return UserResponse.from(user);
     }
 
     @Override
-    public Optional<UserEntity> findBySerialId(String serialId) {
-        return userJpaRepository.findUserIdAndRoleBySerialId(serialId)
-                .flatMap(userSecurityForm -> userJpaRepository.findById(userSecurityForm.getId()));
+    public UserResponse findBySerialId(String serialId) {
+        UserEntity user = userJpaRepository.findUserIdAndRoleBySerialId(serialId)
+                .flatMap(userSecurityForm -> userJpaRepository.findById(userSecurityForm.getId()))
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        return UserResponse.from(user);
+    }
+
+    @Override
+    @Transactional
+    public void updatePassword(Long userId, String newPassword) {
+        UserEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        String encodedPassword = passwordEncoder.encode(newPassword);
+        user.updatePassword(encodedPassword);
+        userJpaRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public UserResponse updateNickname(Long userId, String newNickname) {
+        UserEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.updateInfo(newNickname);
+        return UserResponse.from(userJpaRepository.save(user));
     }
 
     @Override
     @Transactional
     public void updateRefreshToken(Long userId, String refreshToken, boolean isLogin) {
         userJpaRepository.updateRefreshTokenAndLoginStatus(userId, refreshToken, isLogin);
+    }
+
+    @Override
+    @Transactional
+    public void deleteUser(Long userId) {
+        UserEntity user = userJpaRepository.findById(userId)
+                .orElseThrow(() -> new IllegalArgumentException("User not found"));
+        user.delete();
+        userJpaRepository.save(user);
+    }
+
+    @Override
+    @Transactional
+    public UserResponse reActivateUser(String serialId, String newPassword) {
+        UserEntity user = userJpaRepository.findBySerialIdAndIsDeleted(serialId, true)
+                .orElseThrow(() -> new IllegalArgumentException("User not found or already active"));
+        user.reactivate(passwordEncoder.encode(newPassword));
+        return UserResponse.from(userJpaRepository.save(user));
     }
 }
