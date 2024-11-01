ef97173314c1d197e5c4fb3bb4acf17044fb88c1
supportlaver <ak4686@naver.com>
2024-11-01 18:00:29 +0900

    test: 테스트 컨트롤러 추가 및 test 코드는 Filter 에서 제거하도록 설정

---
+++ src/main/java/com/idle/weather/test/TestController.java
@@ -0,0 +1,21 @@
+package com.idle.weather.test;
+
+import com.idle.weather.board.api.port.BoardService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@RequestMapping("/test")
+@RequiredArgsConstructor
+@RestController @Slf4j
+public class TestController {
+
+    private final BoardService boardService;
+    // 투표 추가 및 변경
+    @PostMapping(path = "/{boardId}/vote/{userId}")
+    public void addVote(@PathVariable Long boardId, @PathVariable Long userId ,
+                        @RequestBody TestVoteRequestType voteType) {
+        log.info("1");
+        boardService.addVote(userId, boardId, voteType.getVoteType());
+    }
+}
