02a4463dbfb90c03c789a961f2d5aa27cb0ff413
Shortood <woobin929@naver.com>
2024-11-11 04:08:09 +0000

    fix: 502 bad gateway 오류 수정

--- src/main/java/com/idle/weather/common/constant/Constants.java
+++ src/main/java/com/idle/weather/common/constant/Constants.java
@@ -7,6 +7,7 @@ public class Constants {
     public static final String AUTHORIZATION_HEADER = "Authorization";
     public static final String[] NO_NEED_AUTH_URLS = {
             "/api/v1/auth/basic", "/api/v1/sign-in", "/api/auth/kakao", "/oauth2/authorization/kakao",
-            "/login/oauth2/code/kakao", "/api/v1/auth/reissue", "/api/v1/users/signup", "/api/boards/**"
+            "/login/oauth2/code/kakao", "/api/v1/auth/reissue", "/api/v1/users/signup", "/api/boards/**",
+            "/actuator/health"
     };
 }
