e16f0b08e96ba060e8c067879741993f3e82bac3
supportlaver <ak4686@naver.com>
2024-11-12 15:00:55 +0900

    fix: 실제 AI 서버 주소로 수정

--- src/main/java/com/idle/weather/missionhistory/repository/AIServerImpl.java
+++ src/main/java/com/idle/weather/missionhistory/repository/AIServerImpl.java
@@ -10,6 +10,7 @@ import com.idle.weather.mission.domain.CurrentWeatherResponse;
 import com.idle.weather.missionhistory.service.port.AIServerClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
@@ -25,16 +26,20 @@ public class AIServerImpl implements AIServerClient {
 
     private final RestTemplate restTemplate;
     private final ObjectMapper objectMapper;
-    private static final String AI_END_POINT = "http://localhost:8000";
-    /**
-     * 우선은 번갈아 가면서 성공 , 실패가 나오도록 한다.
-     */
+
+    @Value("${ai.endpoints.mission-auth}")
+    private String aiMissionAuthEndpoints;
+
+    @Value("${ai.endpoints.weather}")
+    private String aiWeatherEndpoints;
+    private final static String AI_END_POINT = "http://localhost:8000/";
+
     @Override
     public boolean missionAuthentication(MissionAuth missionAuthDto) throws JsonProcessingException {
         HttpHeaders headers = new HttpHeaders();
         headers.setContentType(MediaType.APPLICATION_JSON);
         HttpEntity<MissionAuth> request = new HttpEntity<>(missionAuthDto, headers);
-        String response = restTemplate.postForObject(AI_END_POINT + "/verification", request, String.class);
+        String response = restTemplate.postForObject(AI_END_POINT + "verification", request, String.class);
         JsonNode jsonNode = objectMapper.readTree(response);
         log.info("AI Response = {} " , jsonNode.get("certified"));
         return jsonNode.get("certified").asBoolean();
@@ -46,8 +51,7 @@ public class AIServerImpl implements AIServerClient {
         headers.setContentType(MediaType.APPLICATION_JSON);
         WeatherRequest weatherRequest = WeatherRequest.of(latitude, longitude, userId);
         HttpEntity<WeatherRequest> request = new HttpEntity<>(weatherRequest, headers);
-        String response = restTemplate.postForObject(AI_END_POINT + "/weather_data", request, String.class);
-
+        String response = restTemplate.postForObject(AI_END_POINT + "weather_data", request, String.class);
         log.info("response = {} " ,response);
         return objectMapper.readValue(response, WeatherResponse.class);
     }
